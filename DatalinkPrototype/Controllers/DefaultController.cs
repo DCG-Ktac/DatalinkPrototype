127dca7b841ed83217dff3d155d9e84e2a787ff5
Josh Jones <joshuajones05@hotmail.co.uk>
2018-02-02 12:14:36 +0000

    Apparently some other files need commiting idk

---
+++ DatalinkPrototype/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess;
+using System.Data.Entity;
+
+namespace DatalinkPrototype.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        //public IEnumerable<MainTable> Get()
+        //{
+        //    using (TESTEntities entities = new TESTEntities())
+        //    {
+        //        return entities.MainTables.ToList();
+        //    }
+        //}
+
+        [HttpGet]
+        public IEnumerable<MainTable> GetAll()
+        {
+            TESTEntities accessModule = new TESTEntities();
+            
+            return accessModule.MainTables.ToList();
+        }
+    }
+}
